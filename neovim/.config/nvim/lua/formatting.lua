f379360922a823b5d7231dbfa25fb099354b5bf9
David Rans <david@ifixit.com>
2024-01-18 11:23:24 -0700

    phpcbf: disable cause its not working right

--- neovim/.config/nvim/lua/formatting.lua
+++ neovim/.config/nvim/lua/formatting.lua
@@ -1,25 +1,25 @@
 -- Formatters
 -- Formatting can be run via :Format
-local formatter = require('formatter')
+-- local formatter = require('formatter')
 
-formatter.setup {
-  logging = true,
-  filetype = {
-    php = {
-      function ()
-        return {
-          exe = './vendor/bin/phpcbf',
-          args = {'--stdin-path=' .. vim.api.nvim_buf_get_name(0), '-'},
-          stdin = true,
-          ignore_exitcode = true,
-        }
-      end
-    }
-  }
-}
+-- formatter.setup {
+--   logging = true,
+--   filetype = {
+--     php = {
+--       function ()
+--         return {
+--           exe = './vendor/bin/phpcbf',
+--           args = {'--stdin-path=' .. vim.api.nvim_buf_get_name(0), '-'},
+--           stdin = true,
+--           ignore_exitcode = true,
+--         }
+--       end
+--     }
+--   }
+-- }
 
-vim.api.nvim_create_augroup("__formatter__", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePost", {
-	group = "__formatter__",
-	command = ":FormatWrite",
-})
+-- vim.api.nvim_create_augroup("__formatter__", { clear = true })
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+-- 	group = "__formatter__",
+-- 	command = ":FormatWrite",
+-- })
