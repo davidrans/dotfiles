f92a4ba558de053612e436ca6248756a68e89a24
David Rans <dave.g.rans@gmail.com>
2025-05-15 10:33:35 -0600

    Update add some stuff to wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,6 +9,12 @@ config.keys = {
   { key = 'v', mods = 'CTRL', action = act.PasteFrom 'PrimarySelection' },
   -- paste from the clipboard
   { key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
+  { key = 'v', mods = 'SHIFT|CTRL', action = wezterm.action_callback(function(window, pane)
+    window:perform_action(wezterm.action.SendKey{ key='v', mods='CTRL' }, pane) end),
+  },
+  { key = 'V', mods = 'SHIFT|CTRL', action = wezterm.action_callback(function(window, pane)
+    window:perform_action(wezterm.action.SendKey{ key='v', mods='CTRL' }, pane) end),
+  },
 }
 
 -- Use the defaults as a base
@@ -19,5 +25,10 @@ table.insert(config.hyperlink_rules, {
   regex = [[#(\d{2,})]],
   format = 'https://github.com/iFixit/ifixit/issues/$1',
 })
+
+-- Turn off ligatures
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+
+config.selection_word_boundary = " │\t\n{}[]()\"'`|"
 
 return config
